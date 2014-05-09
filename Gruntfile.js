4478ffc175b4de617349786b1a4e861f7aaf6b5b
Jehu <email@marco-michely.de>
2014-05-09 09:50:54 +0200

    added grunt plugins "autoprefixer" and "clean"; enhanced Gruntfile to use new plugins.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,66 @@ module.exports = function(grunt){
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
+    // compile LESS to CSS
 		less: {
 			development: {
 				options: {
 					paths: ["less"],
-					sourceMap: true
+					sourceMap: false
 				},
-				files: {
-					"css/style.css" : "less/style.less"
-				}
-			} 
+				dev: {
+        options: {
+            sourceMap: true
+          },
+          files: {
+            "css/style.css": "less/style.less"
+          }
+        },
+        prod: {
+          files: {
+            "css/style.css": "less/style.less"
+          }
+        }
+			}
 		},
-
+    // automatically add prefixes
+    autoprefixer: {
+      options: {
+        browsers: ["> 1%", "last 2 versions", "last 2 Android versions"],
+        diff: true,
+        expand: true
+      },
+      dev: {
+        files: [
+          {
+            src: 'css/style.css'
+          }
+        ]
+      },
+      prod: {
+        options: {
+          diff: false
+        },
+        files: [
+          {
+            expand: true,
+            src: 'css/style.css'
+          }
+        ]
+      }
+    },
+    // clear a directory before build
+    clean: {
+      all: ['css/*','!**/README.txt']
+    },
 		watch: {
 			options: {
 				spawn: true,
-				interrupt: true
+				interrupt: false
 			},
 			less: {
 				files: ['less/*.less'],
-				tasks: ['less']
+				tasks: ['less:dev','autoprefixer:dev']
 			},
 			livereload: {
 				files: ['css/*.css','templates/**/*.tpl.php'],
@@ -36,6 +75,8 @@ module.exports = function(grunt){
 		}
 	});
 
-	grunt.registerTask('default', ['less']);
+	grunt.registerTask('dev', ['less:dev']);
+  grunt.registerTask('prod', ['clean','less:prod','autoprefixer:prod']);
+  grunt.registerTask('default', ['clean','dev']);
 
 };
